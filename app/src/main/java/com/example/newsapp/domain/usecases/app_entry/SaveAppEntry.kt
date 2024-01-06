e821eb0c64cfd5a7af9cc6d8717081d7f3e0f958
Pranav Salunke <pranavsalunke2021@gmail.com>
2024-01-07 00:45:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/domain/usecases/app_entry/SaveAppEntry.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp.domain.usecases.app_entry
+
+import com.example.newsapp.domain.manger.LocalUserManger
+import javax.inject.Inject
+
+class SaveAppEntry @Inject constructor(
+    private val localUserManger: LocalUserManger
+) {
+
+    suspend operator fun invoke(){
+        localUserManger.saveAppEntry()
+    }
+
+}
